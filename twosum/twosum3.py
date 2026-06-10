c4158fd5c73207de646d3c37f21b655a7a0080eb
Alan Abbott <alabbott@pm.me>
2026-06-10 10:04:47 -0500

    hash map two sum

---
+++ twosum/twosum3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        index = 0
+        for num in nums:
+            if num in hash_map.keys():
+                return [index, hash_map[num]]
+            complement = target - num
+            hash_map[complement] = index
+            index += 1
